968610d630139e0312bda98752c1fb5f98aedc0d
bhavya <rkumarbhavy@gmail.com>
2024-07-24 02:51:13 +0530

    Added frame for button and textbox

--- GUI/tkinter_test.py
+++ GUI/tkinter_test.py
@@ -1,17 +1,24 @@
 import tkinter as tk
 from tkinter import font
 
-# Definitions
+#---Definitions---
 root = tk.Tk()
 root.title("Lisa (Llama 2 7b)")
 root.geometry("400x540")
 
-# Layout
+#---Layout---
 heading = tk.Label(root, text="Lisa 👩🏻‍💻", font=(font.BOLD, 20))
 heading.pack_configure(pady=20)
 
-textbox = tk.Text(root, height=3, font=("Helvetica", 20, "bold"), borderwidth=2, relief=tk.SOLID)
-textbox.pack_configure(side=tk.BOTTOM, padx=3, pady=3)
+#frame for button and textbox
+frame = tk.Frame(root)
+frame.pack(side=tk.BOTTOM, fill=tk.X, padx=10, pady=10)
+
+send_button = tk.Button(frame, text="Send 📩", font=(font.BOLD, 15))
+send_button.pack_configure(side=tk.RIGHT, padx=7)
+
+textbox = tk.Text(frame, height=2, font=("Helvetica", 20, "bold"), borderwidth=2, relief=tk.SOLID)
+textbox.pack_configure(side=tk.LEFT, padx=3, pady=3)
 
 
 root.mainloop()
