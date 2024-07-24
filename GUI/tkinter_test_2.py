8309609f52541625198a13896adb6c07e2b376c0
bhavya <rkumarbhavy@gmail.com>
2024-07-25 01:17:11 +0530

    Completed GUI Changes

--- GUI/tkinter_test_2.py
+++ GUI/tkinter_test_2.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import font
+import ai_chat
 
 
 class App:
@@ -13,11 +14,14 @@ class App:
         self.heading = tk.Label(self.root, text="Lisa 👩🏻‍💻", font=(font.BOLD, 20))
         self.heading.pack_configure(pady=20)
 
+        self.voice_button = tk.Button(self.root, text="🎙️", font=(font.BOLD, 15), command=self.send_message)
+        self.voice_button.pack_configure(pady=100)
+
         #frame for button and textbox
         self.frame = tk.Frame(self.root)
         self.frame.pack(side=tk.BOTTOM, fill=tk.X, padx=10, pady=10)
 
-        self.send_button = tk.Button(self.frame, text="Send 📩", font=(font.BOLD, 15))
+        self.send_button = tk.Button(self.frame, text="Send 📩", font=(font.BOLD, 15), command=self.send_message)
         self.send_button.pack_configure(side=tk.RIGHT, padx=7)
 
         self.textbox = tk.Text(self.frame, height=2, font=("Helvetica", 20, "bold"), borderwidth=2, relief=tk.SOLID)
@@ -28,10 +32,15 @@ class App:
 
     def shortcut(self, event):
         if event.keysym == 'Return' and event.state == 0:
-            print('h')
+            self.send_message()
 
-            "----TO:DO----"
+    def send_message(self):
+        message = self.textbox.get(1.0, tk.END)
+        print(message)
+        "----TO:DO----"
+        # ai_chat.chat()
 
-            self.textbox.delete(1.0, tk.END)
+        self.textbox.delete(1.0, tk.END)
+
 
 App()
